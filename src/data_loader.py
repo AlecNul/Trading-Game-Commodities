c653d2ef86245ac607d92ea795a06c30a8547bce
AlecNul <reynen.alec@gmail.com>
2026-02-20 17:33:46 +0100

    Feat: Cointegration test

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,31 +2,31 @@ import json
 import yfinance as yf
 import os
 
-# Path handling
-script_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(script_dir)
-data_config_path = os.path.join(project_root, 'config', 'data_config.json')
-data_dir = os.path.join(project_root, 'data')
+def load_csv(start_date="2020-01-01", end_date="2025-12-31", write=False):
+    # Path handling
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(script_dir)
+    data_config_path = os.path.join(project_root, 'config', 'data_config.json')
+    data_dir = os.path.join(project_root, 'data')
 
-# Ticker Configuration
-with open(data_config_path, 'r') as f:
-    config = json.load(f)
+    # Ticker Configuration
+    with open(data_config_path, 'r') as f:
+        config = json.load(f)
 
-all_tickers = [config['futures']] + config['etfs'] + config['equities']
-start_date = config['start_date']
+    all_tickers = config['futures'] + config['etfs'] + config['equities'] + config['forex'] + config['softs']
 
-# Data Downloading
-print("Downloading...")
-data = yf.download(all_tickers, start=start_date, interval="1d", auto_adjust=False) # We'll use 2 headers for the CSV
+    # Data Downloading
+    # print("Downloading data...")
+    data = yf.download(all_tickers, start=start_date, end=end_date, interval="1d", auto_adjust=False) # We'll use 2 headers for the CSV
 
-df_final = data.dropna()
+    if write:
+        # CSV Creation
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
 
-# CSV Creation
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
+        output_path = os.path.join(data_dir, 'dataset_G21.csv')
+        data.to_csv(output_path)
 
-output_path = os.path.join('data', 'dataset_G21.csv')
-df_final.to_csv(output_path)
-
-print(f"Dataset added in data")
-print(f"Dataset size : {df_final.shape}")
+    # print(f"Done.")
+    # print(f"Dataset size : {df_final.shape}")
+    return data
